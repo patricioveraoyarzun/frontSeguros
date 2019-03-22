526600b4f1b6fe541ec0e5ac2b8ae90e050d8ae6
patricioveraoyarzun <48566836+patricioveraoyarzun@users.noreply.github.com>
2019-03-21 23:50:05 -0300

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  APIEndpoint: 'http://192.168.1.251:8080/api/',
+  APIEndpointAuth: 'http://192.168.1.251:8080/',
+  validarSesion: false,
+  enableDebug: true
+};
